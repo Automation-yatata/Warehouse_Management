f8cb64ece00cfffc8d269d3d145f330b3b453938
Jose Rodrigues <up201806386@edu.fe.up.pt>
2021-12-20 11:29:17 +0000

    Replace Supplier.java

--- src/Supplier.java
+++ src/Supplier.java
@@ -5,13 +5,11 @@ class Supplier{
 	private String Name;
 	private String Contact;
 	private String Email;
-	private Location Loc;
 
-	public Supplier(String name, String contact, String email, Location loc){
+	public Supplier(String name, String contact, String email){
 		this.Name = name;
 		this.Contact = contact;
 		this.Email = email;
-		this.Loc = loc;
 	}
 
 	public String getName(){
@@ -26,11 +24,7 @@ class Supplier{
 		
 		return this.Email;
 	}
-	public Location getLocation(){
-		
-		return this.Loc;
-	}
-
+	
 	public int setName(User u, String newName){
 		//verificar se user é Admin
 		/*
@@ -58,14 +52,6 @@ class Supplier{
 		this.Email = newEmail;
 		return 0;
 	}
-	public int setLocation(User u, Location newLocation){
-		//verificar se user é Admin
-		/*
-			return -1 caso não seja
-		*/
-
-		this.Loc = newLocation;
-		return 0;
-	}
+	
 
 }
